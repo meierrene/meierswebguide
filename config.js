51afe6470af84edb969e2e7333f2308be736cfa1
meierrene <renemeier.de@gmail.com>
2023-01-18 14:50:36 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+// export const year = document.querySelector('.year');
+// export const date = document.querySelector('.date-display');
+// export const flags = document.querySelector('.flags');
+// export const h1 = document.querySelector('h1');
+// export const h2 = document.querySelector('h2');
+// export const title1 = document.querySelector('.column-1-title');
+// export const title2 = document.querySelector('.column-2-title');
+// export const title3 = document.querySelector('.column-3-title');
+// export const title4 = document.querySelector('.column-4-title');
+// export const column3 = document.querySelector('.column-3');
+// export const column4 = document.querySelector('.column-4');
+// export const secretBtn = document.querySelector('.secret-btn');
+// export const footer = document.querySelector('.footer-text');
+// export const now = new Date();
+// export const options = {
+//   day: 'numeric',
+//   month: 'numeric',
+//   year: 'numeric',
+//   weekday: 'long',
+// };
