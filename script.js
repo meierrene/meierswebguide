3769de34d6ccb51b80adc432d29711e55990f12e
René Meier <renemeier.de@gmail.com>
2024-09-10 17:41:44 -0300

    new changes

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const year = document.querySelector('.year');
+const column2 = document.querySelector('.column-2');
 const column3 = document.querySelector('.column-3');
 const column4 = document.querySelector('.column-4');
 const secretBtn = document.querySelector('.secret-btn');
 
 secretBtn.addEventListener('click', () => {
   if (secretBtn.checked) {
+    column2.classList.remove('hidden');
     column3.classList.remove('hidden');
     column4.classList.remove('hidden');
   } else {
+    column2.classList.add('hidden');
     column3.classList.add('hidden');
     column4.classList.add('hidden');
   }
